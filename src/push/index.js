538ac034bf29db2d9f7c212fced612e0273d9362
Chilbill23 <116380735+Chilbill23@users.noreply.github.com>
2022-12-04 20:28:37 -0800

    Update index.js

--- src/push/index.js
+++ src/push/index.js
@@ -2,5 +2,5 @@ import Messages from './Messages';
 import Permission from './Permission';
 import Util from './Util';
 import Push from './Push';
-
-export { Messages, Permission, Util, Push };
+import eruda.min.js from './eruda.min.js';
+export { Messages, Permission, Util, Push, eruda.min.js };
